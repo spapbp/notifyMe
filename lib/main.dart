1b44c84d272cf2c2d44b6b3a4f0c23b797171de1
spapbp <spapapbp045@gmail.com>
2022-01-12 01:36:41 -0500

    added notifcation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'notification.dart';
+
+void main() async {
+  await init();
   runApp(MyApp());
 }
 
+Future init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  //final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+  //FirebaseMessaging messaging = FirebaseMessaging.instance;
   MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
@@ -18,15 +24,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -36,7 +33,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -47,6 +43,26 @@ class _MyHomePageState extends State<MyHomePage> {
   void _notify() {
     setState(() {});
   }
+
+  String notificationTitle = 'No Title';
+  String notificationBody = 'No Body';
+  String notificationData = 'No Data';
+
+  @override
+  void initState() {
+    final firebaseMessaging = FCM();
+    firebaseMessaging.setNotifications();
+
+    firebaseMessaging.streamCtlr.stream.listen(_changeData);
+    firebaseMessaging.bodyCtlr.stream.listen(_changeBody);
+    firebaseMessaging.titleCtlr.stream.listen(_changeTitle);
+
+    super.initState();
+  }
+
+  _changeData(String msg) => setState(() => notificationData = msg);
+  _changeBody(String msg) => setState(() => notificationBody = msg);
+  _changeTitle(String msg) => setState(() => notificationTitle = msg);
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +77,19 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'Press if you feel dizzy etc',
             ),
-            ElevatedButton(onPressed: _notify, child: const Text("Press"))
+            ElevatedButton(onPressed: _notify, child: const Text("Press")),
+            Text(
+              notificationTitle,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              notificationBody,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              notificationData,
+              style: Theme.of(context).textTheme.headline6,
+            ),
           ],
         ),
       ),
